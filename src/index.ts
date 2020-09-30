8e09b82204194cea1282338ca12bdddb4eadcade
Al-amin Yusuf <alaminyusuf131@gmail.com>
2020-09-30 14:55:48 +0100

    created schema and root query

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,14 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+// const { buildSchema } = require('graphql');
 
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+import schema from './schema/schema';
 
 const app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true,
   })
 );
--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,1 +1,38 @@
-import { GraphQLObjectType } from 'graphql';
+import {
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+
+// demo data
+const users = [
+  { id: '1', name: 'Adrean', email: 'adrean@gmail.com' },
+  { id: '2', name: 'Mike', email: 'mike@gmail.com' },
+  { id: '3', name: 'Paul', email: 'paul@gmail.com' },
+];
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    users: {
+      type: new GraphQLList(UserType),
+      resolve() {
+        return users;
+      },
+    },
+  },
+});
+
+export default new GraphQLSchema({
+  query: RootQuery,
+});
